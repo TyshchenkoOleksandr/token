8fd90f3195867fa37320eda57d85645980a66e19
Oleksandr Tyshchenko <tushchenkooleksandr@gmail.com>
2022-02-08 12:17:56 +0200

    add formik

--- front/src/components/loginForm/LoginForm.tsx
+++ front/src/components/loginForm/LoginForm.tsx
@@ -1,73 +1,65 @@
-import React, { ChangeEvent, FC, useState, MouseEvent } from 'react';
+import React, { FC, FormEvent, ReactElement } from 'react';
+import { Formik } from 'formik';
 
 import useUser from '../../providers/userProvider/useUser';
 import useStylesLoginForm from './loginForm.theme';
+import { initialFormikValues } from './loginFormConfig';
 
 const LoginForm: FC = () => {
   const classes = useStylesLoginForm();
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const {
-    login,
-    registration,
-  } = useUser();
+  const { login, registration } = useUser();
 
-  const changeLogin = (event: ChangeEvent<HTMLInputElement>): void => {
-    const newValue = event.target.value;
-    setEmail(newValue);
-  };
-
-  const changePassword = (event: ChangeEvent<HTMLInputElement>): void => {
-    const newValue = event.target.value;
-    setPassword(newValue);
-  };
-
-  const onLoginButtonClick = (event: MouseEvent<HTMLButtonElement>): void => {
-    event.preventDefault();
-
+  const onLoginButtonClick = (email: string, password: string): void => {
     login(email, password);
   };
 
-  const onRegisterButtonClick = (event: MouseEvent<HTMLButtonElement>): void => {
-    event.preventDefault();
-
+  const onRegisterButtonClick = (email: string, password: string) => (): void => {
     registration(email, password);
   };
 
   return (
-    <form className={classes.loginForm}>
-      <input
-        type='text'
-        placeholder='Enter your email'
-        value={email}
-        onChange={changeLogin}
-        className={classes.loginFormInput}
-      />
-
-      <input
-        type='password'
-        placeholder='Enter your password'
-        value={password}
-        onChange={changePassword}
-        className={classes.loginFormInput}
-      />
-      <div className={classes.loginFormButtonBox}>
-        <button
-          type='button'
-          onClick={onLoginButtonClick}
+    <Formik
+      initialValues={initialFormikValues}
+      onSubmit={(): void => {}}
+    >
+      {(props): ReactElement => (
+        <form className={classes.loginForm} onSubmit={(event: FormEvent<HTMLFormElement>): void => {
+          event.preventDefault();
+          onLoginButtonClick(props.values.email, props.values.password);
+        }}
         >
-          login
-        </button>
+          <input
+            type='text'
+            name='email'
+            placeholder='Enter your email'
+            value={props.values.email}
+            onChange={props.handleChange}
+            className={classes.loginFormInput}
+          />
 
-        <button
-          type='button'
-          onClick={onRegisterButtonClick}
-        >
-          register
-        </button>
-      </div>
+          <input
+            type='password'
+            name='password'
+            placeholder='Enter your password'
+            value={props.values.password}
+            onChange={props.handleChange}
+            className={classes.loginFormInput}
+          />
+          <div className={classes.loginFormButtonBox}>
+            <button type='submit'>
+              login
+            </button>
 
-    </form>
+            <button
+              type='button'
+              onClick={onRegisterButtonClick(props.values.email, props.values.password)}
+            >
+              register
+            </button>
+          </div>
+        </form>
+      )}
+    </Formik>
   );
 };
 
